07d0d7233f8e665c5bb6998e46f3a80a6a6f051e
homya <homyak11122@outlook.com>
2024-11-14 23:12:27 +0300

    Add TokenService realization

--- User.API/User.Infrastructure/Services/TokenService.cs
+++ User.API/User.Infrastructure/Services/TokenService.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Users.Application.Interfaces;
+using Users.Domain.Entities;
 
 namespace Users.Infrastructure.Services
 {
-    internal class TokenService
+    internal class TokenService : ITokenService
     {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var cres = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(double.Parse(_configuration["Jwt:ExpireMinutes"])),
+                signingCredentials: cres);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
     }
 }
