d379bafe80dec1eb20b32385463c3add952dfe25
homya <homyak11122@outlook.com>
2024-12-19 14:04:44 +0300

    Messages.API  created but without realization

---
+++ Messages.API/Messages.Domain/Entities/Message.cs
@@ -0,0 +1,14 @@
+﻿using Share.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Messages.Domain.Entities;
+
+public class Message : BaseEntity
+{
+    public string Content { get; set; }
+    [Required]
+    public int ProfileId { get; set; }
+    public DateTime Timestamp { get; set; }
+    [Required]
+    public int EventId { get; set; }
+}
